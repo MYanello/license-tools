6a2dbd162e6aa25c4ab4c56e490a68653054aec0
Marcus Yanello <marcus@yanello.net>
2023-12-14 19:15:17 -0600

    moving things

--- check_avail/tests/conftest.py
+++ check_avail/tests/conftest.py
@@ -8,15 +8,16 @@ from app import main
 @pytest.fixture
 def setup(): #setup the application with a small cache size and ttl
     args = argparse.Namespace(redis_host = '127.0.0.1', redis_port = '6379', password = 'rescale', size = 1, ttl = 1)
-    application = app.redis_proxy(args)
+    application = True #app.redis_proxy(args)
     yield application
     application.clean()
 
 @pytest.fixture # A fixture to set up and tear down test data cache
 def mock_cached_data():
-    app.cached_data = {'cached_key': b'Cached Data'}
-    yield app.cached_data
-    app.cached_data.clear()
+    #app.cached_data = {'cached_key': b'Cached Data'}
+    return(True)
+    #yield app.cached_data
+    #app.cached_data.clear()
 
 @pytest.fixture # A fixture to mock the redis get method so we can test without a redis server
 def mock_redis(mocker):
